056990ad4fa4e2b294f4b7357ca975c9f8eb7021
Lyret <viktor@lyresten.se>
2024-06-25 13:58:48 +0200

    Adds Manage page currently being worked on

---
+++ ui/pages/Manage.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { blur } from "svelte/transition";
+	import { openPanels } from "~/stores/ui";
+	import { createAPIStore } from "~/stores";
+	import Header from "../components/content/Header.svelte";
+	
+	const participants = createAPIStore("participant");
+	
+</script>
+
+<!-- Header -->
+<Header />
+<!-- Content -->
+<main class="p-3 container is-widescreen" in:blur={{ delay: 0, duration: 1000 }}>
+	<div class="columns">
+		{#if $openPanels["live"]}
+			<div class="column">Live</div>
+		{/if}
+		{#if $openPanels["scenes"]}
+			<div class="column">Scenes</div>
+		{/if}
+		{#if $openPanels["participants"]}
+			<div class="column">
+				Deltagare
+				{JSON.stringify({participants: $participants}, null, 4)}
+			</div>
+		{/if}
+		{#if $openPanels["chatt"]}
+			<div class="column">Chatt</div>
+		{/if}
+	</div>
+</main>
+<div class="is-hidden-touch">
+	<div style="border-bottom: 1px solid hsl(0, 0%, 86%); width: 100vw; -webkit-font-smoothing: antialiased;"/>
+	<p class="is-plants has-text-centered is-size-4">a b c d e f g h i j k l m n o p q r s t u v w x y z y x w v u t s r q p o m n l k j i h g f e</p>
+</div>
