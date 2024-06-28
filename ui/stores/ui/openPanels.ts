d62d04e92d7974c1c53a209bdd21b0eb085e8ca6
Lyret <viktor@lyresten.se>
2024-06-28 11:25:59 +0200

    Adds open panels store

---
+++ ui/stores/ui/openPanels.ts
@@ -0,0 +1,31 @@
+import { writable, derived } from 'svelte/store'
+
+let nonStoreValue: { [panel: string]: boolean } =
+  localStorage
+    .getItem('open-panes')
+    ?.split(';')
+    .reduce((obj, val) => (val ? { [val]: true, ...obj } : obj), {}) || {}
+
+const writableOpenPanels = writable<{ [panel: string]: boolean }>(nonStoreValue)
+
+function serializeOpenPanels() {
+  const serializedValue = Object.keys(nonStoreValue).join(';')
+  localStorage.setItem('open-panes', serializedValue)
+}
+
+export const openPanels = derived(
+  [writableOpenPanels],
+  ([$writableOpenPanels]) => {
+    return $writableOpenPanels
+  }
+)
+
+export function togglePanel(panel: string) {
+  if (nonStoreValue[panel]) {
+    delete nonStoreValue[panel]
+  } else {
+    nonStoreValue[panel] = true
+  }
+  serializeOpenPanels()
+  writableOpenPanels.set(nonStoreValue)
+}
