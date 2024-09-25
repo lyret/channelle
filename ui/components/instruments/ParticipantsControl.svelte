cfe08cbd049102f5ee334f6e15416fa0cc89a89b
Lyret <viktor@lyresten.se>
2024-09-25 17:08:28 +0200

    Updates Participation instruments

--- ui/components/instruments/ParticipantsControl.svelte
+++ ui/components/instruments/ParticipantsControl.svelte
@@ -2,10 +2,12 @@
 	import type { DataTypes } from '~/api';
 	import { update } from '~/api';
 	import { clickOutside } from '~/directives';
+	import { userCameraBans, userMicrophoneBans } from '~/stores/users';
 
 	export let participant: DataTypes['participant'];
+	export let online: boolean = false;
 	let loading: boolean = false;
-	let dropdownActive: boolean = false;
+	let active: boolean = false;
 
 	async function doUpdate(
 		participant: DataTypes['participant'],
@@ -24,219 +26,226 @@
 	}
 </script>
 
-<div class="level">
-	<div class="level-item">
+<div
+	class="list-item"
+	use:clickOutside
+	on:click_outside={() => (active = false)}
+>
+	<div class="accordion-header" class:is-loading={loading}>
 		<div
-			class="dropdown"
-			class:is-active={dropdownActive}
-			style="width: 100%;"
-			use:clickOutside
-			on:click_outside={() => (dropdownActive = false)}
+			class="icon"
+			class:has-text-grey-light={!online}
+			class:has-text-success={online}
 		>
-			<div class="dropdown-trigger">
-				<button
-					class="button"
-					aria-haspopup="true"
-					aria-controls="dropdown-menu"
-					on:click={() => (dropdownActive = true)}
-					class:is-loading={loading}
-				>
-					<span
-						class="icon"
-						class:has-text-grey-light={!participant.online}
-						class:has-text-success={participant.online}
-					>
-						{#if participant.manager}
-							<ion-icon name="build"></ion-icon>
-						{/if}
-						{#if participant.actor}
-							<ion-icon name="accessibility"></ion-icon>
-						{/if}
-						{#if !participant.manager && !participant.actor}
-							<ion-icon name="ellipse"></ion-icon>
-						{/if}
-					</span>
-					<span class="pr-7" class:is-strikethrough={participant.blocked}
-						>{participant.name}</span
-					>
-				</button>
-			</div>
-			<div class="dropdown-menu">
-				<div class="dropdown-content">
-					<!-- ALLOW AUDIO/VIDEO -->
-					<div class="dropdown-item">
-						<div class="buttons has-addons level-item">
-							<button
-								class="button is-small"
-								class:has-text-danger={!participant.allowedVideo}
-								on:click={() =>
-									doUpdate(participant, {
-										allowedVideo: !participant.allowedVideo,
-									})}
-							>
-								<span class="icon">
-									<ion-icon
-										name={participant.allowedVideo
-											? 'videocam'
-											: 'videocam-off'}
-									></ion-icon>
-								</span>
-							</button>
-							<button
-								class="button is-small"
-								class:has-text-danger={!participant.allowedAudio}
-								on:click={() =>
-									doUpdate(participant, {
-										allowedAudio: !participant.allowedAudio,
-									})}
-							>
-								<span class="icon">
-									<ion-icon name={participant.allowedAudio ? 'mic' : 'mic-off'}
-									></ion-icon>
-								</span>
-							</button>
-						</div>
-					</div>
-					<!-- MAKE ACTOR -->
-					{#if participant.actor}
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<!-- svelte-ignore a11y-no-static-element-interactions -->
-						<a
-							class="dropdown-item"
-							on:click={() =>
-								doUpdate(
-									participant,
-									{ actor: false },
-									`Ta bort "${participant.name}" som skådespelare?`
-								)}
-						>
-							<span class="icon"><ion-icon name="close"></ion-icon></span> Inte skådespare
-						</a>
-					{:else}
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<!-- svelte-ignore a11y-no-static-element-interactions -->
-						<a
-							class="dropdown-item"
-							on:click={() =>
-								doUpdate(
-									participant,
-									{ actor: true },
-									`Är "${participant.name}" en skådespelare?`
-								)}
-						>
-							<span class="icon"
-								><ion-icon name="accessibility"></ion-icon></span
-							> Skådespelare
-						</a>
-					{/if}
-					<!-- MAKE TECHNICAN -->
-					{#if participant.manager}
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<!-- svelte-ignore a11y-no-static-element-interactions -->
-						<a
-							class="dropdown-item"
-							on:click={() =>
-								doUpdate(
-									participant,
-									{ manager: false },
-									`Ta bort "${participant.name}" som tekniker?`
-								)}
-						>
-							<span class="icon"><ion-icon name="close"></ion-icon></span> Inte tekniker
-						</a>
-					{:else}
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<!-- svelte-ignore a11y-no-static-element-interactions -->
-						<a
-							class="dropdown-item"
-							on:click={() =>
-								doUpdate(
-									participant,
-									{ manager: true },
-									`Är "${participant.name}" en tekniker?`
-								)}
-						>
-							<span class="icon"><ion-icon name="build"></ion-icon></span> Tekniker
-						</a>
-					{/if}
-					<!-- CHANGE NAME -->
-					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<!-- svelte-ignore a11y-missing-attribute -->
-					<!-- svelte-ignore a11y-no-static-element-interactions -->
-					<a
-						class="dropdown-item"
-						on:click={() =>
-							doUpdate(participant, {
-								name:
-									prompt('Skriv nytt namn', participant.name) ||
-									participant.name,
-							})}
-					>
-						<span class="icon"><ion-icon name="text"></ion-icon></span> Byt namn
-					</a>
-					<!-- BLOCK -->
-					{#if participant.blocked}
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<!-- svelte-ignore a11y-no-static-element-interactions -->
-						<a
-							class="dropdown-item"
-							on:click={() => doUpdate(participant, { blocked: false })}
-						>
-							<span class="icon"><ion-icon name="lock-open"></ion-icon></span> Tillåt
-							tillbaka
-						</a>
-					{:else}
-						<!-- svelte-ignore a11y-click-events-have-key-events -->
-						<!-- svelte-ignore a11y-missing-attribute -->
-						<!-- svelte-ignore a11y-no-static-element-interactions -->
-						<a
-							class="dropdown-item"
-							on:click={() =>
-								doUpdate(
-									participant,
-									{ blocked: true },
-									`Vill du blockera och ta bort "${participant.name}"?`
-								)}
-						>
-							<span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-							Blockera person
-						</a>
-					{/if}
-				</div>
-			</div>
+			{#if participant.manager}
+				<ion-icon name="build"></ion-icon>
+			{:else if participant.actor}
+				<ion-icon name="accessibility"></ion-icon>
+			{:else}
+				<ion-icon name="ellipse"></ion-icon>
+			{/if}
 		</div>
-	</div>
-	<div class="level-item">
-		<div class="buttons has-addons level-item">
+		<div class="name pr-7" class:is-strikethrough={participant.blocked}>
+			{participant.name}
+		</div>
+		<div class="buttons">
+			{#if participant.manager || participant.actor}
+				<button
+					class="button is-small"
+					class:has-text-danger={$userCameraBans[participant.id]}
+					on:click={() =>
+						userCameraBans.set(
+							participant.id,
+							!$userCameraBans[participant.id]
+						)}
+				>
+					<span class="icon">
+						<ion-icon
+							name={$userCameraBans[participant.id]
+								? 'videocam-off'
+								: 'videocam'}
+						></ion-icon>
+					</span>
+				</button>
+			{/if}
 			<button
 				class="button is-small"
-				class:has-text-danger={!participant.allowedVideo}
+				class:has-text-danger={$userMicrophoneBans[participant.id]}
 				on:click={() =>
-					doUpdate(participant, { allowedVideo: !participant.allowedVideo })}
+					userMicrophoneBans.set(
+						participant.id,
+						!$userMicrophoneBans[participant.id]
+					)}
 			>
 				<span class="icon">
 					<ion-icon
-						name={participant.allowedVideo ? 'videocam' : 'videocam-off'}
+						name={$userMicrophoneBans[participant.id] ? 'mic-off' : 'mic'}
 					></ion-icon>
 				</span>
 			</button>
-			<button
-				class="button is-small"
-				class:has-text-danger={!participant.allowedAudio}
-				on:click={() =>
-					doUpdate(participant, { allowedAudio: !participant.allowedAudio })}
-			>
+			<button class="button is-small" on:click={() => (active = !active)}>
 				<span class="icon">
-					<ion-icon name={participant.allowedAudio ? 'mic' : 'mic-off'}
-					></ion-icon>
+					{#if active}
+						<ion-icon name="close"></ion-icon>
+					{:else}
+						<ion-icon name="options"></ion-icon>
+					{/if}
 				</span>
 			</button>
 		</div>
 	</div>
+	{#if active}
+		<div class="accordion-content">
+			<!-- MAKE ACTOR -->
+			{#if participant.actor && !participant.manager}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<a
+					class="dropdown-item"
+					on:click={() =>
+						doUpdate(
+							participant,
+							{ actor: false },
+							`Ta bort "${participant.name}" som skådespelare?`
+						)}
+				>
+					<span class="icon"><ion-icon name="close"></ion-icon></span> Inte skådespare
+				</a>
+			{:else if !participant.manager}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<a
+					class="dropdown-item"
+					on:click={() =>
+						doUpdate(
+							participant,
+							{ actor: true },
+							`Är "${participant.name}" en skådespelare?`
+						)}
+				>
+					<span class="icon"><ion-icon name="accessibility"></ion-icon></span>
+					Skådespelare
+				</a>
+			{/if}
+			<!-- MAKE TECHNICAN -->
+			{#if participant.manager}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<a
+					class="dropdown-item"
+					on:click={() =>
+						doUpdate(
+							participant,
+							{ manager: false },
+							`Ta bort "${participant.name}" som tekniker?`
+						)}
+				>
+					<span class="icon"><ion-icon name="close"></ion-icon></span> Inte tekniker
+				</a>
+			{:else}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<a
+					class="dropdown-item"
+					on:click={() =>
+						doUpdate(
+							participant,
+							{ manager: true },
+							`Är "${participant.name}" en tekniker?`
+						)}
+				>
+					<span class="icon"><ion-icon name="build"></ion-icon></span> Tekniker
+				</a>
+			{/if}
+			<!-- CHANGE NAME -->
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-missing-attribute -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<a
+				class="dropdown-item"
+				on:click={() =>
+					doUpdate(participant, {
+						name:
+							prompt('Skriv nytt namn', participant.name) || participant.name,
+					})}
+			>
+				<span class="icon"><ion-icon name="text"></ion-icon></span> Byt namn
+			</a>
+			<!-- BLOCK -->
+			{#if participant.blocked}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<a
+					class="dropdown-item"
+					on:click={() => doUpdate(participant, { blocked: false })}
+				>
+					<span class="icon"><ion-icon name="lock-open"></ion-icon></span> Tillåt
+					tillbaka
+				</a>
+			{:else}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<a
+					class="dropdown-item"
+					on:click={() =>
+						doUpdate(
+							participant,
+							{ blocked: true },
+							`Vill du blockera och ta bort "${participant.name}"?`
+						)}
+				>
+					<span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+					Blockera person
+				</a>
+			{/if}
+		</div>
+	{/if}
 </div>
-<hr />
+
+<style>
+	.list-item {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-items: start;
+		justify-content: start;
+		align-items: start;
+		align-content: start;
+	}
+	.accordion-header {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-items: start;
+		justify-content: start;
+		align-items: start;
+		align-content: start;
+	}
+	.accordion-header div {
+		display: block;
+		flex-grow: 0;
+		flex-shrink: 1;
+		flex-basis: auto;
+		align-self: auto;
+		order: 0;
+	}
+	.accordion-header .name {
+		flex-grow: 1;
+	}
+
+	.accordion-content {
+		flex-basis: 1;
+		width: 100%;
+	}
+
+	.accordion-content a:hover {
+		color: white;
+	}
+</style>
--- ui/components/instruments/ParticipantsInstrument.svelte
+++ ui/components/instruments/ParticipantsInstrument.svelte
@@ -1,58 +1,125 @@
 <script lang="ts">
 	import { createDatabaseStore } from '~/stores';
+	import { onlineStatus } from '~/stores/users';
 	import ParticipantsControl from './ParticipantsControl.svelte';
 
 	const participants = createDatabaseStore('participant');
 	$: participantsWithName = $participants.filter((p) => p.name && !p.blocked);
 	$: managers = participantsWithName.filter((p) => p.manager);
-	$: actors = participantsWithName.filter((p) => p.actor);
+	$: actors = participantsWithName.filter((p) => p.actor && !p.manager);
 	$: visitors = participantsWithName.filter(
 		(p) => !p.manager && !p.actor && !p.blocked
 	);
 	$: blocked = $participants.filter((p) => p.blocked && p.name);
+	$: online = participantsWithName.filter((p) => $onlineStatus[p.id]);
+
+	let filter: string = 'Deltagare';
 </script>
 
-<h1 class="title is-size-3">Deltagare</h1>
-
-<div class="field has-addons">
-	<p class="control">
-		<button class="button is-small">
-			<span>Tekniker</span>
-		</button>
-	</p>
-	<p class="control">
-		<button class="button is-small">
-			<span>Deltagare</span>
-		</button>
-	</p>
-	<p class="control">
-		<button class="button is-small">
-			<span>Blockerade</span>
-		</button>
-	</p>
+<div class="radios is-size-7">
+	<label class="radio">
+		<input type="radio" name="filter" value="Deltagare" bind:group={filter} />
+		Alla
+	</label>
+	{#if managers.length}
+		<label class="radio has-text-info">
+			<input type="radio" name="filter" value="Tekniker" bind:group={filter} />
+			Tekniker
+		</label>
+	{/if}
+	{#if actors.length}
+		<label class="radio has-text-info">
+			<input
+				type="radio"
+				name="filter"
+				bind:group={filter}
+				value="Skådespelare"
+			/>
+			Skådespelare
+		</label>
+	{/if}
+	<label class="radio has-text-success">
+		<input
+			type="radio"
+			name="filter"
+			value="Deltagare online"
+			bind:group={filter}
+		/>
+		Online
+	</label>
+	{#if blocked.length}
+		<label class="radio has-text-danger">
+			<input
+				type="radio"
+				name="filter"
+				value="Blockerade deltagare"
+				bind:group={filter}
+			/>
+			Blockerade
+		</label>
+	{/if}
 </div>
 
-{#if managers.length}
-	<h2 class="title is-size-6 mb-3 mt-4">Tekniker</h2>
-	{#each managers as participant}
-		<ParticipantsControl {participant} />
-	{/each}
-{/if}
-{#if actors.length}
-	<h2 class="title is-size-6 mb-3 mt-8">Skådespelare</h2>
-	{#each actors as participant}
-		<ParticipantsControl {participant} />
-	{/each}
-{/if}
-{#if visitors.length}
-	<h2 class="title is-size-6 mb-3 mt-8">Besökare</h2>
-	{#each visitors as participant}
-		<ParticipantsControl {participant} />
-	{/each}
-{/if}
-{#if blocked.length}
-	<h2 class="title is-size-6 mb-3 mt-8">Blockerade</h2>
-	{#each blocked as participant}
-		<ParticipantsControl {participant} />
-	{/each}
-{/if}
+<h1
+	class="title is-size-3 mt-5"
+	class:has-text-info={filter == 'Tekniker' || filter == 'Skådespelare'}
+	class:has-text-success={filter == 'Deltagare online'}
+	class:has-text-danger={filter == 'Blockerade deltagare'}
+>
+	{filter}
+</h1>
+
+<div class="list">
+	{#if blocked.length && filter == 'Blockerade deltagare'}
+		{#each blocked as participant}
+			{#key participant.id}
+				<ParticipantsControl
+					{participant}
+					online={$onlineStatus[participant.id]}
+				/>
+			{/key}
+		{/each}
+	{:else if online.length && filter == 'Deltagare online'}
+		{#each online as participant}
+			{#key participant.id}
+				<ParticipantsControl
+					{participant}
+					online={$onlineStatus[participant.id]}
+				/>
+			{/key}
+		{/each}
+	{:else}
+		{#if managers.length && (filter == 'Deltagare' || filter == 'Tekniker')}
+			{#each managers as participant}
+				{#key participant.id}
+					<ParticipantsControl
+						{participant}
+						online={$onlineStatus[participant.id]}
+					/>
+				{/key}
+			{/each}
+		{/if}
+		{#if actors.length && (filter == 'Deltagare' || filter == 'Skådespelare')}
+			{#each actors as participant}
+				{#key participant.id}
+					<ParticipantsControl
+						{participant}
+						online={$onlineStatus[participant.id]}
+					/>
+				{/key}
+			{/each}
+		{/if}
+		{#if visitors.length && filter != 'Tekniker' && filter != 'Skådespelare'}
+			{#each visitors as participant}
+				{#key participant.id}
+					{#key participant.id}
+						<ParticipantsControl
+							{participant}
+							online={$onlineStatus[participant.id]}
+						/>
+					{/key}
+				{/key}
+			{/each}
+		{/if}
+	{/if}
+</div>
