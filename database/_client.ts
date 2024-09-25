c9f999db4750afdc80c7d52c69af74a6f6d17731
Lyret <viktor@lyresten.se>
2024-09-25 14:39:09 +0200

    Removes plans to support prisma computed attributes

--- database/_client.ts
+++ database/_client.ts
@@ -3,18 +3,5 @@ import { PrismaClient } from '@prisma/client';
 /** Extended database client */
 export const client = new PrismaClient();
 
-// .$extends({
-//   result: {
-//     participant: {
-//       calculatedOnline: {
-//         needs: {},
-//         compute(participant) {
-//           return !!participant.online
-//         },
-//       },
-//     },
-//   },
-// })
-
 /** Client type definition */
 export type Client = typeof client;
