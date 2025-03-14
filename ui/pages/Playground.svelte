cf402a321e4f892ca57ecd6bb98a643991b016ee
Lyret <viktor@lyresten.se>
2025-03-14 14:52:01 +0100

    Adds some information to the playground

--- ui/pages/Playground.svelte
+++ ui/pages/Playground.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
 	import { blur } from "svelte/transition";
+	import { StageAudio } from "~/lib/stores/stageAudio";
+	import { StageLayout } from "~/lib/stores/stageLayout";
+	import { userCameraBans, userMicrophoneBans } from "~/stores/users";
+	import { currentParticipant } from "../lib/stores/api";
+
+	let countOfStageAudio = 0;
+	StageAudio.subscribe(() => countOfStageAudio++);
+	let countOfStageLayout = 0;
+	StageLayout.subscribe(() => countOfStageLayout++);
 </script>
 
 <main in:blur={{ delay: 500, duration: 1000 }}>
-	<h1>Working</h1>
+	<h1>CurrentParticipant</h1>
+	{JSON.stringify($currentParticipant)}
+	<h1>Camera Bans</h1>
+	{JSON.stringify($userCameraBans)}
+	<h1>Microphone Bans</h1>
+	{JSON.stringify($userMicrophoneBans)}
+	<h1>Stage Layout ({countOfStageLayout})</h1>
+	{JSON.stringify($StageLayout)}
+	<h1>Stage Audio ({countOfStageLayout})</h1>
+	{JSON.stringify($StageAudio)}
 </main>
